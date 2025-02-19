db15f36f336eb30eea52bb89a166b928162755de
Daniel Pereira Rodrigues de Lima <daniellima913@hotmail.com>
2025-02-19 12:34:23 -0300

    Lesson 4 finished

---
+++ java-anatomia-classes/src/BoletimEstudantim.java
@@ -0,0 +1,13 @@
+public class BoletimEstudantim {
+    public static void main(String[] args){
+        // lesson four
+        // explicando a idententação
+        int mediaFinal = 7;
+        if(mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if(mediaFinal == 6) 
+            System.out.println("Prova Minerva");
+        else
+            System.out.println("Aprovado");
+    }
+}
