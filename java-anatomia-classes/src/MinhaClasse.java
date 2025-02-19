1f5f7128edc744f738516b09364b04c1fb2b10d0
Daniel Pereira Rodrigues de Lima <daniellima913@hotmail.com>
2025-02-19 11:41:51 -0300

    Lesson 3 finished

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,11 +1,33 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("Olá você que acessou meu gitHub!!");
+        // lesson one
+        System.out.println("Olá você que acessou meu gitHub!!");
 
+        //lesson two
         // essa variavel não pode ser alterada e deve ser escrita de forma maiuscula
         final String BR = "Brasil";
         final Double PI = 3.14;
 
+        String primeiroNome ="Daniel";
+        String segundoNome = "Lima";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }   
+
+    // lesson 3
+
+    // variavel
+    // Tipo NomeBemDefenido = atribuição (que é opcional)
+
+    // metodo
+    // TipoRetorno NomeObjetivoNoInfinitivo (parametro(s))
+    // int somar(int numero Um, int numero2)
+    // String formatarCep(long cep)
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método é " + primeiroNome.concat(" ").concat(segundoNome);            
     }
 }
