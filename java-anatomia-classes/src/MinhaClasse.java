9502c71924fcaa87580b87fdbe2b530f34927263
Daniel Pereira Rodrigues de Lima <daniellima913@hotmail.com>
2025-02-19 11:26:20 -0300

    Lesson 2 finished

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,5 +2,10 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
         System.out.print("Olá você que acessou meu gitHub!!");
+
+        // essa variavel não pode ser alterada e deve ser escrita de forma maiuscula
+        final String BR = "Brasil";
+        final Double PI = 3.14;
+
     }
 }
